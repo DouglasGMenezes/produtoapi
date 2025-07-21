a7a0b2a3843c028c6e69f0fcce70cdcea325683b
Douglas Menezes <douglasgm@MacBook-Air-de-Douglas.local>
2025-07-21 17:30:24 -0300

    aplicacao finalizada.

--- src/main/java/com/douglasgm/produtoapi/controller/ProdutoController.java
+++ src/main/java/com/douglasgm/produtoapi/controller/ProdutoController.java
@@ -3,12 +3,16 @@ package com.douglasgm.produtoapi.controller;
 import com.douglasgm.produtoapi.model.Produto;
 import com.douglasgm.produtoapi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(name = "produto")
+@RequestMapping("produtos")
 public class ProdutoController {
 
     @Autowired
@@ -17,9 +21,39 @@ public class ProdutoController {
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto Recebido" + produto);
-        produtoRepository.save(produto);
 
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
     }
 
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id ){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletar(@PathVariable String id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if (produto.isPresent()) {
+            produtoRepository.deleteById(id);
+            return ResponseEntity.ok("Produto excluido");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
+        }
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> obterPorNome(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNomeContainingIgnoreCase(nome);
+    }
 }
--- src/main/java/com/douglasgm/produtoapi/model/Produto.java
+++ src/main/java/com/douglasgm/produtoapi/model/Produto.java
@@ -1,17 +1,15 @@
 package com.douglasgm.produtoapi.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 
 @Entity
 @Table(name = "produto")
 public class Produto {
 
+
+//    @GeneratedValue(strategy = GenerationType.UUID)
     @Id
-    @Column
     private String id;
     @Column
     private String nome;
